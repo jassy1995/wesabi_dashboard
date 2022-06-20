ded49666ec486cfc6d66577ce520c52d3f79ba6b
jassy1995 <babatundejoseph85@gmail.com>
2022-06-20 11:39:32 +0100

    edited

--- src/components/dropDown.js
+++ src/components/dropDown.js
@@ -26,7 +26,7 @@ export default function DropDown() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
@@ -42,6 +42,8 @@ export default function DropDown() {
                 </Link>
               )}
             </Menu.Item>
+          </div>
+          <div className="py-1">
             <Menu.Item>
               {({ active }) => (
                 <Link to="/customer">
@@ -57,6 +59,8 @@ export default function DropDown() {
                 </Link>
               )}
             </Menu.Item>
+          </div>
+          <div className="py-1">
             <Menu.Item>
               {({ active }) => (
                 <Link to="/skill">
@@ -72,6 +76,8 @@ export default function DropDown() {
                 </Link>
               )}
             </Menu.Item>
+          </div>
+          <div className="py-1">
             <Menu.Item>
               {({ active }) => (
                 <Link to="/unSkill">
